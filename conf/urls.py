565a39e1365665e1c06dd4848825705ca4263393
Ariel Calzada <ariel.calzada@gmail.com>
2021-02-27 15:16:44 -0500

    Restore urls

--- conf/urls.py
+++ conf/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
@@ -24,12 +24,6 @@ from applications.home.views import Home
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
-
-    # home
-    path('', Home.as_view(), name='home'),
-
-    # contact
-    path('/contact', Home.as_view(), name='contact'),
 
     # home
     path('', Home.as_view(), name='home'),
